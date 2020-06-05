c4e7e822046060e4e90a34ee554958acea6c1b7e
OmniFaR <timo.scholli@gmx.de>
2020-06-05 18:57:46 +0200

    Created most basic camera.

---
+++ src/Engine/Camera/Impl/StaticCamera.ts
@@ -0,0 +1,45 @@
+import ICamera from "../ICamera";
+import { injectable } from "inversify";
+import { Application } from "pixi.js";
+import { Engine, Bounds, Vector } from "matter-js";
+
+@injectable()
+class StaticCamera extends ICamera {
+
+  private position: Vector = Vector.create();
+
+  private size: Vector = Vector.create();
+
+  private bounds: Bounds;
+
+  constructor() {
+    super();
+    
+    this.updateBounds();
+  }
+
+  public setPosition(x: number, y: number): StaticCamera {
+    this.position = Vector.create(x, y);
+    this.updateBounds();
+    return this;
+  }
+
+  public setSize(width: number, height: number): StaticCamera {
+    this.size = Vector.create(width, height);
+    this.updateBounds();
+    return this;
+  }
+
+  private updateBounds() {
+    this.bounds = Bounds.create([this.position,Vector.add(this.position, this.size)]);
+  }
+
+  public onUpdate(engine: Engine, app: Application) {
+    this.updateCameraPositionAndSize(this.position, this.size, Vector.create(0, 0));
+  }
+
+  public getBounds(): Bounds {
+    return this.bounds;
+  }
+
+}
